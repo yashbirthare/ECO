08801af3ba5cb91f840967462fb1eebd461a6c26
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-12 20:58:10 +0530

    done

--- src/pages/ProductCard/ProductCard.jsx
+++ src/pages/ProductCard/ProductCard.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./ProductCard.css"
 
-export function ProductCard({price,title,image,description}) {
+export function ProductCard({title,description,price,category,processor,qty,new_arrival,original_price,discount,rating,image }) {
   return (
     <div className="imageList">
   <div>
        <img src={image} alt="" />
     </div>
     <div>{title}</div>
-    <div>{price}</div>
-
-
-    <div>{title}</div>
     <div>{description}</div>
+    <div>{price}</div>
+    <div>{category}</div>
+    <div>{processor}</div>
+    <div>{qty}</div>
+    <div>{new_arrival && <div>new</div> }</div>
+    <div>{original_price}</div>
+    <div>{discount}</div>
+     <div>{rating.rate}</div> 
+     <div>{rating.count}</div>  
+    
     
 
 
