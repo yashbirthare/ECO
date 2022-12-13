9926a5796d97e15665da8dce55e99e29896ca923
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-13 20:39:13 +0530

    done

--- src/pages/ProductCard/ProductCard.jsx
+++ src/pages/ProductCard/ProductCard.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import "./ProductCard.css"
 
-export function ProductCard({title,description,price,category,processor,qty,new_arrival,original_price,discount,rating,image }) {
+export function ProductCard({tittle,description,price,category,processor,qty,new_arrival,original_price,discount,rating,image }) {
   return (
     <div className="item-list">
   <div className="image-tag">
        <img src={image} alt="" />
-    </div>
-    <div className="titel-tag">{title}</div>
-    <div className="description-tag">{description}</div>
-    <div>{price}</div>
-    <div>{category}</div>
-    <div>{processor}</div>
-    <div>{qty}</div>
-    <div>{new_arrival && <div>new</div> }</div>
-    <div>{original_price}</div>
-    <div>{discount}</div>
-     <div>{rating.rate}</div> 
-     <div>{rating.count}</div>  
-
-     <div>
+       
+       <div className="btn-tag"> 
       <button className="Cart-btn">ADD TO CART</button>
       <button className="Buy-btn" >BUY NOW</button>
+      </div>
+    </div>
+
+    
+    <div>
+    <p className="titel-tag">{tittle}</p>
+    <p className="description-tag">{description}</p>
+    <p>{price}</p>
+    <p>{category}</p>
+    <p>{processor}</p>
+    <p>{qty}</p>
+    <p>{new_arrival && <div>new</div> }</p>
+    <p>{original_price}</p>
+    <p>{discount}</p>
+     <p>{rating.rate}</p> 
+     <p>{rating.count}</p>  
      </div>
+
+  
+     
      
     </div>
   );
