2b10e4cb2dd1d6de1ad0487151df0e5216ddd82e
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-18 20:21:35 +0530

    done

--- src/pages/ProductCard/ProductCard.jsx
+++ src/pages/ProductCard/ProductCard.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./ProductCard.css"
 
  function ProductCard({tittle,description,price,category,processor,qty,new_arrival,original_price,discount,rate,count,image }) {
+
   return (
     <div id="item-list">
   <div>
   <p>{new_arrival && <div className="product-new">new</div> }</p>
-       <img src={image} alt="" />
+       <img className="image-items" src={image} alt="" />
        
        <div className="btn-tag"> 
       <button className="Cart-btn">ADD TO CART</button>
@@ -18,17 +19,30 @@ import "./ProductCard.css"
     <div id="productCard-main">
     <p className="titel-tag">{tittle}</p>
     <p className="description-tag">{description}</p>
-    <p className="product-price">{price}</p>
-    <div className="all-Items">
+    <div className="last-Item">
     <p>{category}</p>
     <p>{processor}</p>
-    <p>{qty}</p>
+    </div>
 
-    <p>{original_price}</p>
-    <p>{discount}</p>
-     <p>{rate}</p>/ 
-     <p>{count}</p>
-     </div>  
+<div id="rating-main">
+    <div className="rating">
+    <p>{count}</p>
+    <p>.{rate}☆</p>
+  </div>
+
+  <p className="Reviews">783 Ratings & 76 Reviews</p>
+</div>
+    <div id="price">
+    <p className="pro-price">{price}</p>
+    <s> <p className="ori-price">{original_price}</p></s>
+    <p className="dis-price">{discount}%off</p>
+  
+    </div>
+    <p className="qty">Hurry, Only {qty} left!</p>
+
+   
+    
+
      </div>
     </div>
   );
