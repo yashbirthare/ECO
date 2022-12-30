870bc00ed84466453d77817d0544f8673d942128
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-12-31 00:41:11 +0530

    Fotter work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import React from 'react'
-import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 import { Routes,Route } from "react-router-dom";
 import ProductStore from "./pages/ProductStore/ProductStore"
 import {ProductDetail} from "./pages/ProductDetail/ProductDetail";
@@ -22,6 +23,8 @@ const App = () => {
    <Route path="/ProductStore" element={<ProductStore />}/>
    <Route path="/ProductDetail/:productId" element={<ProductDetail />}/>
   </Routes>
+   
+   <div><Footer/></div>
 
     </div>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { AiFillGift} from 'react-icons/ai';
 import { FaMapMarkerAlt} from 'react-icons/fa';
 
 import React from 'react'
-import "./header.css"
+import "./Header.css"
 
 
 const header = () => {
