6087f4fe379928622fe37f6b7e6450949f02bbf2
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2023-01-04 20:28:00 +0530

    done

--- src/pages/ProductStore/ProductStore.jsx
+++ src/pages/ProductStore/ProductStore.jsx
@@ -10,8 +10,9 @@ const ProductStore  = () => {
     
     <div>
     <div id="ProductStore-Main-Area"> {products.map((product) => 
+
+
     <div>
-     
      <p>{product.new_arrival && <div className="ProductStore-Item-new">new</div>}</p>
     <Link to={`/ProductDetail/${product.id}`}> <img className='ProductStore-Img1-Main' src={product.image} alt="/"/></Link>
     <div/> 
@@ -31,16 +32,19 @@ const ProductStore  = () => {
    <p className="ProductStore-tittle-tag">{product.tittle}</p>
    <p className="ProductStore-description-tag">{product.description}</p>
    </div>
-          
+   <div className="ProductStore-Price-Main">
+   <p className="ProductStore-pro-price">{product.price}</p>
+   <s> <p className="ProductStore-ori-price">{product.original_price}</p> </s>
+   <p className="ProductStore-dis-price">{product.discount}%off</p>
+ </div>  
 
-         <div className="ProductStore-Price-Main">
-            <p className="ProductStore-pro-price">{product.price}</p>
-            <s> <p className="ProductStore-ori-price">{product.original_price}</p> </s>
-            <p className="ProductStore-dis-price">{product.discount}%off</p>
-         </div>   
-        
-            </div>)} 
-            
+ <div className="ProductStore-Main-BTN"> 
+      <button className="ProductStore-Cart-btn">ADD TO CART</button>
+      <button className="ProductStore-Buy-btn" >BUY NOW</button>
+      </div>
+
+</div>)} 
+    
       </div>
       </div>
   )
